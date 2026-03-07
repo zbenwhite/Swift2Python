18b7b2fce0edd9ca3b22d4f42d0dd476f988fd02
Ben White <benjamin.l.white@icloud.com>
2026-03-07 07:31:46 -0800

    Automatic PythonObject conversions

--- Sources/Swift2Python/Conversions.swift
+++ Sources/Swift2Python/Conversions.swift
@@ -5,30 +5,63 @@
 //  Created by Ben White on 3/2/26.
 //
 
-protocol PendingPythonConvertible {
+public protocol PendingPythonConvertible: Sendable {
     func toPythonObject(interpreter: PythonInterpreter) async throws -> PythonObject
 }
 
 extension Bool: PendingPythonConvertible {
-    func toPythonObject(interpreter: PythonInterpreter) async throws -> PythonObject {
+    public func toPythonObject(interpreter: PythonInterpreter) async throws -> PythonObject {
         try await interpreter.convertBoolToPython(self)
     }
 }
 
 extension Double: PendingPythonConvertible {
-    func toPythonObject(interpreter: PythonInterpreter) async throws -> PythonObject {
+    public func toPythonObject(interpreter: PythonInterpreter) async throws -> PythonObject {
         try await interpreter.convertDoubleToPython(self)
     }
 }
 
 extension Int: PendingPythonConvertible {
-    func toPythonObject(interpreter: PythonInterpreter) async throws -> PythonObject {
+    public func toPythonObject(interpreter: PythonInterpreter) async throws -> PythonObject {
         try await interpreter.convertIntToPython(self)
     }
 }
 
 extension String: PendingPythonConvertible {
-    func toPythonObject(interpreter: PythonInterpreter) async throws -> PythonObject {
+    public func toPythonObject(interpreter: PythonInterpreter) async throws -> PythonObject {
         try await interpreter.convertStringToPython(self)
+    }
+}
+
+extension Array : PendingPythonConvertible where Element : PendingPythonConvertible {
+    public func toPythonObject(interpreter: PythonInterpreter) async throws -> PythonObject {
+        try await interpreter.convertArrayToPython(self)
+    }
+}
+
+extension Dictionary : PendingPythonConvertible where Key : PendingPythonConvertible & Hashable, Value : PendingPythonConvertible {
+    public func toPythonObject(interpreter: PythonInterpreter) async throws -> PythonObject {
+        try await interpreter.convertDictionaryToPython(self)
+    }
+}
+
+extension Range : PendingPythonConvertible where Bound : PendingPythonConvertible {
+    public func toPythonObject(interpreter: PythonInterpreter) async throws -> PythonObject {
+        fatalError("shut up xcode")
+        //try await interpreter.convertRangeToPython(self)
+    }
+}
+
+extension PartialRangeFrom : PendingPythonConvertible where Bound : PendingPythonConvertible {
+    public func toPythonObject(interpreter: PythonInterpreter) async throws -> PythonObject {
+        fatalError("shut up xcode")
+        //try await interpreter.convertPartialRangeToPython(self)
+    }
+}
+
+extension PartialRangeUpTo : PendingPythonConvertible where Bound : PendingPythonConvertible {
+    public func toPythonObject(interpreter: PythonInterpreter) async throws -> PythonObject {
+        fatalError("shut up xcode")
+        //try await interpreter.convertPartialRangeToPython(self)
     }
 }
